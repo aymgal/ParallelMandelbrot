d32bfa12ba6b15b96c28d2a651c776869629faf2
Aymeric <aymgal@users.noreply.github.com>
2017-05-31 16:15:56 +0200

    Split serial tests into separate files

---
+++ analysis/cluster/srl_fix_N_200.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#------------------------------ weak scaling ---------------------------------#
+
+sdir=slurm_runs_N
+
+n_iter=200
+
+outfile=srl_N_10000_${n_iter}_1_1_1.dat
+echo "Output file name : " $outfile
+
+mkdir -p outputs
+
+rm -f outputs/$outfile
+rm -f out_*.pgm
+rm -f out_*.bmp
+
+cd ../../src 			# go there to compile
+make srl
+cd ../analysis/cluster	# come back here to run
+
+### problem size doubles when number of processors doubles
+### --> N multiplied by sqrt(2)
+
+N=10000
+
+echo "Going to run several times mandel_srl, with parameters N = $N, \
+max iter = $n_iter, rows = 1, procs = 1, threads = 1)"
+
+sbatch -n 1 ${sdir}/run_srl_N_${N}_${n_iter}.slurm
+
+
+#-----------------------------------------------------------------------------#
+
