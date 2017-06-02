d296678c2ad3f4abdc4c346b30a96901fa2f4159
Aymeric <aymgal@users.noreply.github.com>
2017-06-02 23:56:16 +0200

    Add hybrid weak scaling with 8 threads/rank

---
+++ analysis/cluster/hyb_weak_scaling_8.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#------------------------------ weak scaling ---------------------------------#
+
+# vary number of rows of the division, to be equal to N
+
+sdir=slurm_runs_weak
+
+n_iter=100
+n_thread=8 # per rank
+
+outfile=hyb_weak_x_${n_iter}_x_x_${n_thread}.dat
+echo "Output file name : " $outfile
+
+mkdir -p outputs
+
+rm -f outputs/$outfile
+rm -f out_*.pgm
+rm -f out_*.bmp
+
+cd ../../src 			# go there to compile
+make hyb
+cd ../analysis/cluster	# come back here to run
+
+### problem size doubles when number of processors doubles
+### --> N multiplied by sqrt(2)
+
+n_proc=2
+
+# for N in 1024 1448 2048 2896 4096 5793
+for N in 2048 2896 4096 5793 # not enough resources to go up to 64 ranks + 4 threads/rank...
+do
+	n_row=$N # division in rows equal to N (total of N rows of 1 pixel thick)
+
+	echo "Going to run several times mandel_hyb, with parameters N = $N, \
+max iter = $n_iter, rows = $n_row, procs = $n_proc, threads = $n_thread)"
+    
+	sbatch -n $n_proc -c $n_thread ${sdir}/run_hyb_weak_${N}_${n_iter}_${n_row}_${n_thread}.slurm
+
+	((n_proc = n_proc * 2))
+done
+
+#-----------------------------------------------------------------------------#
+
