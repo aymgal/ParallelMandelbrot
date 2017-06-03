5f276ef64f1dd68087978be27a8516e08ce16c93
Aymeric <aymgal@users.noreply.github.com>
2017-06-03 09:33:09 +0200

    Add hybrid code strong scaling for 8 threads/rank + add 12 proc to have one more data point

--- analysis/cluster/hyb_strong_scaling_5000_8.sh
+++ analysis/cluster/hyb_strong_scaling_5000_8.sh
@@ -9,7 +9,7 @@ sdir=slurm_runs_strong
 N=5000
 n_iter=100
 n_row=5000  # for best scaling
-n_thread=4
+n_thread=8
 
 outfile=hyb_strong_${N}_${n_iter}_${n_row}_x_${n_thread}.dat
 echo "Output file name : " $outfile
@@ -24,7 +24,7 @@ cd ../../src 			# go there to compile
 make hyb
 cd ../analysis/cluster	# come back here to run
 
-for n_proc in 2 4 8 12 16
+for n_proc in 2 4 8 12
 do
 	echo "Going to run several times mandel_hyb, with parameters N = $N, \
 max iter = $n_iter, rows = $n_row, procs = $n_proc, threads = $n_thread)"
